b2ef8aa499216e04d6c3df22bba0853f4f9a5b3d
Nermin Kaya <nermin.kaya.sau@gmail.com>
2019-12-18 11:28:36 +0300

    Editing Files

--- hotel/urls.py
+++ hotel/urls.py
@@ -4,10 +4,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 
-    path('hotel/', views.hotel),
-    path('show/', views.show),
-    path('edit/', views.edit),
-    path('update/', views.update),
-    path('delete/', views.destroy),
+
 
 ]
